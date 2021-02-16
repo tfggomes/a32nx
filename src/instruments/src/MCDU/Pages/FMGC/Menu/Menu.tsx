6a9d556fba95cc9eee8a29a579a1b186b00c0dc7
Alex McBean <al.mcbean@outlook.com>
2021-02-16 12:16:32 +0000

    Components Extracted to TSX

--- src/instruments/src/MCDU/Pages/FMGC/Menu/Menu.tsx
+++ src/instruments/src/MCDU/Pages/FMGC/Menu/Menu.tsx
@@ -1,17 +1,23 @@
-import React, {FunctionComponent, useContext, useState} from 'react';
+import React, {FunctionComponent, useContext, useEffect, useState} from 'react';
 import {Column, column_sides} from "../../../Components/Column";
 import {LineType} from "../../../Components/Line";
+import { RootContext } from "../../../RootContext.jsx"
 import "./stlyes.scss"
 
 type MenuProps = {
 
 }
-const MenuPage: FunctionComponent<MenuProps> = ({}) => {
+const MenuPage: FunctionComponent<MenuProps> = () => {
     const [l1, setl1] = useState<LineType>({field: {text: "FMGC",}});
     const [l2, setl2] = useState<LineType>({field: {text: "ATSU",}});
     const [l3, setl3] = useState<LineType>({field: {text: "AIDS",}});
     const [l4, setl5] = useState<LineType>({field: {text: "CFDS",}});
     const [r6, setr6] = useState<LineType>({field: {text: "Return",}});
+    const [, , , setTitle] = useContext(RootContext);
+
+    useEffect(() => {
+        setTitle("MCDU MENU")
+    }, []);
 
     return (
         <div className="menu-contents" >
--- src/instruments/src/MCDU/index.jsx
+++ src/instruments/src/MCDU/index.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import { useReducer, useState } from 'react';
+import { useState } from 'react';
 import {
     renderTarget,
     useUpdate,
